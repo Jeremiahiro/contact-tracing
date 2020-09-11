f9bdae2493ce4c5d1011a5256a5f11b30c29cd96
NielEche <danieleche7@gmail.com>
2020-09-11 15:54:15 +0100

    FEAT: Added correct paths to images in the serviceworker.js file

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -3,14 +3,14 @@ var filesToCache = [
     '/offline',
     '/css/app.css',
     '/js/app.js',
-    '/images/icons/icon-72x72.png',
-    '/images/icons/icon-96x96.png',
-    '/images/icons/icon-128x128.png',
-    '/images/icons/icon-144x144.png',
-    '/images/icons/icon-152x152.png',
-    '/images/icons/icon-192x192.png',
-    '/images/icons/icon-384x384.png',
-    '/images/icons/icon-512x512.png',
+    '../frontend/img/icons/icon-72x72.png',
+    '../frontend/img/icons/icon-96x96.png',
+    '../frontend/img/icons/icon-128x128.png',
+    '../frontend/img/icons/icon-144x144.png',
+    '../frontend/img/icons/icon-152x152.png',
+    '../frontend/img/icons/icon-192x192.png',
+    '../frontend/img/icons/icon-384x384.png',
+    '../frontend/img/icons/icon-512x512.png',
 ];
 
 // Cache on install
