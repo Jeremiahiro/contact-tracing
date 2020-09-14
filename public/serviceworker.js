44d77ff144d828c59deb4263cbd53fdfa7721e3d
jeremiahiro <jeremiahiro@gmail.com>
2020-09-14 15:27:42 +0100

    fix: serviceworker.js

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,9 +1,10 @@
 var staticCacheName = "pwa-v" + new Date().getTime();
 var filesToCache = [
-    '/css/app.css',
-    '/js/app.js',
+    // '/login',
     '../frontend/bootstrap/css/bootstrap.css.map',
     '../frontend/css/splash.css',
+    '../frontend/css/custom.css',
+    '../frontend/js/script.js',
     '../frontend/img/cont.png',
     '../frontend/img/cont1.png',
     '../frontend/img/cont2.png',
